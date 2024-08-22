1d8021637e2b881b66bdbd9c07d6e09b0f9fb669
sukmaajidigital <114824269+sukmaajidigital@users.noreply.github.com>
2024-08-22 14:55:26 +0700

    add dumping

--- dumpingfromtable.py
+++ dumpingfromtable.py
@@ -3,7 +3,8 @@ from bs4 import BeautifulSoup
 import mysql.connector
 
 # URL dari file HTML lokal
-url = "https://dashboard-lldikti6.kemdikbud.go.id/?791c9464d7028b0a6ba3be88194eb13e"  # Ganti dengan URL atau path file HTML lokal
+# url = "https://dashboard-lldikti6.kemdikbud.go.id/?791c9464d7028b0a6ba3be88194eb13e"  # jawa tengah
+url = "https://direktori.lldikti4.id/perguruantinggi/listsatuanpendidikan/" # jabar banten
 
 # Membuka file HTML lokal
 response = requests.get(url)
@@ -23,18 +24,23 @@ mydb = mysql.connector.connect(
 
 mycursor = mydb.cursor()
 
-# Loop melalui baris tabel, melewatkan header
 for row in rows[1:]:  # Lewati baris header
     cols = row.find_all('td')
-    KodePerguruanT = cols[1].text.strip()
-    NamaPerguruanT = cols[2].text.strip()
-    Kota = cols[3].text.strip()
+    
+    # Cek jumlah kolom sebelum mengaksesnya
+    if len(cols) >= 9:
+        col1 = cols[1].text.strip()
+        col2 = cols[2].text.strip()
+        col3 = cols[7].text.strip()
+        col4 = cols[8].text.strip()
 
-    # Masukkan data ke database
-    mycursor.execute(
-        "INSERT INTO perguruan_tinggi (KodePerguruanT, NamaPerguruanT, Kota) VALUES (%s, %s, %s)", 
-        (KodePerguruanT, NamaPerguruanT, Kota)
-    )
+        # Masukkan data ke database
+        mycursor.execute(
+            "INSERT INTO perguruan_tinggi (KodePerguruanT, NamaPerguruanT, Kota, Provinsi) VALUES (%s, %s, %s, %s)", 
+            (col1, col2, col3, col4)
+        )
+    else:
+        print("Baris ini tidak memiliki cukup kolom:", row)
 
 # Commit dan tutup koneksi
 mydb.commit()
